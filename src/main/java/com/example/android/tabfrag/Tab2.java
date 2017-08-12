acaeab2e22ea803cdce4a00a2265a5dbf5f966ce
skaranraju <karans.raju@gmail.com>
2017-08-12 13:00:50 +0530

    Add files via upload

---
+++ src/main/java/com/example/android/tabfrag/Tab2.java
@@ -0,0 +1,21 @@
+package com.example.android.tabfrag;
+
+/**
+ * Created by karan on 12-08-2017.
+ */
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Tab2 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2, container, false);
+
+        return rootView;
+    }
+}
---
+++ src/main/java/com/example/android/tabfrag/Tab3.java
@@ -0,0 +1,22 @@
+package com.example.android.tabfrag;
+
+/**
+ * Created by karan on 12-08-2017.
+ */
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Tab3 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3, container, false);
+
+
+        return rootView;
+    }
+}
